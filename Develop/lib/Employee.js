5e97579be5939f024600dac307f923815d7800f8
Fabian <fabianffc@gmail.com>
2021-01-17 17:01:45 -0600

    setting up directory structure

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,37 @@
+// TODO: Write code to define and export the Employee class
+class Employee{
+        constructor(type,brand){
+        }
+
+
+
+
+        
+// var dog={
+//     name:"wolfy",
+//     haircolor:"brown"
+// }
+
+// var cat={
+//     name:"meow",
+//     haircolor:"black"
+// }
+// 
+//    getBrand(){
+ //   return this.brand
+//    }
+    //   getType(){
+    //      return this.type 
+    //   }
+// }
+
+// class Animal extends Food{
+//     constructor(name,haircolor,type,brand){
+//         super(type,brand)
+//         this.name=name
+//         this.haircolor=haircolor
+//     }
+// }
+
+// var dog= new Animal("wolfy","brown","dog","purina")
+// var cat= new Animal("meow","black","cat","iAMs")
