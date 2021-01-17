e9738777372f28b08b814eaf4d8e6d5d788763b5
Fabian <fabianffc@gmail.com>
2021-01-17 17:23:37 -0600

    DS store in gitignore and methods for employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,37 +1,26 @@
 // TODO: Write code to define and export the Employee class
-class Employee{
-        constructor(type,brand){
-        }
+// class Employee properties for (name,id,email)
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email
+    };
+    //methods
+    getName() {
+        return this.name
+    };
+    getId() {
+        return this.id
+    };
+    getEmail() {
+        return this.email
+    };
+    getRole() {
+        return Employee
+    };
 
 
-
-
-        
-// var dog={
-//     name:"wolfy",
-//     haircolor:"brown"
-// }
-
-// var cat={
-//     name:"meow",
-//     haircolor:"black"
-// }
-// 
-//    getBrand(){
- //   return this.brand
-//    }
-    //   getType(){
-    //      return this.type 
-    //   }
-// }
-
-// class Animal extends Food{
-//     constructor(name,haircolor,type,brand){
-//         super(type,brand)
-//         this.name=name
-//         this.haircolor=haircolor
-//     }
-// }
 
 // var dog= new Animal("wolfy","brown","dog","purina")
 // var cat= new Animal("meow","black","cat","iAMs")
