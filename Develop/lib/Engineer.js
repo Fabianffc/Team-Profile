bf05dda7e68a5f8131e8306b0f2081e7e513c34b
Fabian <fabianffc@gmail.com>
2021-01-17 18:54:57 -0600

    setting up the class Manager & Engineer inheriting the subclasses from class Employee

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,21 @@
 // TODO: Write code to define and export the Engineer class. 
 // HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        this.officeNumber = officeNumber;
+        //super() is called to avoid duplicating the constructor parts' that are common between Employee and Engineer
+        super(name, id, email);
+        this.github = github;
+    }
+    //methods 
+    getGithub(){
+        return github
+    }
+    getRole() {
+        return "Manager"
+    };
+};
+//exporting the class Engineer
+module.exports = Engineer;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,13 +1,19 @@
 // TODO: Write code to define and export the Manager class. 
 //HINT: This class should inherit from Employee.
 const Employee = require("./Employee.js");
+//Defining the "Manager" class using the "Employee" classes to not repeat code
 
-class Manager extends Employee{
-    constructor(name, id, email, officeNumber){
-        this.officeNumber = this.officeNumber;
-    }
-
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        this.officeNumber = officeNumber;
+        //super() is called to avoid duplicating the constructor parts' that are common between Employee and Manager
+        super(name, id, email);
+    };
+    //methods
+    getRole() {
+        return "Manager"
+    };
 };
 
-
-
+//exporting the class Manager
+module.exports = Manager;
